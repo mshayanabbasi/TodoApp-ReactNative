8c17952b48f021ec2cefb3c795fa6b9971ae4fdf
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-10 00:18:16 +0500

    Todo Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoScreen from './screens/TodoScreen';
+
+const App = () => {
+  console.disableYellowBox = true;
+  return (
+    <>
+      <TodoScreen />
+    </>
+  );
+};
+
+export default App;
