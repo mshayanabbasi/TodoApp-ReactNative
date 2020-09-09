56c28b75df8466ce707a4869b89aa7ae6704918e
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-10 00:19:38 +0500

    add Todos

--- src/services/index.js
+++ src/services/index.js
@@ -4,4 +4,7 @@ export const getTodos = async () => {
   const res = await axios.get('/todo');
   return res.data;
 };
+export const createTodo = async (payload) => {
+  return await axios.post('/todo', payload);
+};
 
